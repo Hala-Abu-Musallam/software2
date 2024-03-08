f1696f5bfd10d118c485655f3889863c8b5f9b1a
Hala-Abu-Musallam <halakhaled2002g2@gmail.com>
2024-03-08 23:46:13 +0200

    login

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,31 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        User user = new User();
+while(true) {
+    System.out.println("choose 1 for login , 2 for sign up:");
+    int userinput = scanner.nextInt();
+    if (userinput == 1) {
         System.out.println("enter username");
-        String username;
-        String password;
 
+        String username = scanner.next();
+        System.out.println("enter password");
+        String password = scanner.next();
+        user.login(username, password);
+    } else {
+        System.out.println("enter username");
+        String username = scanner.next();
+        System.out.println("enter password");
+        String password = scanner.next();
+        user.login(password, username);
+    }
+}
 
     }
+
+
 }
