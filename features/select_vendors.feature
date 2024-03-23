ab64f573038d37663115dd6623275412f2a45495
Hala-Abu-Musallam <halakhaled2002g2@gmail.com>
2024-03-23 21:06:04 +0200

    login

--- features/select_vendors.feature
+++ features/select_vendors.feature
@@ -1,12 +1,13 @@
 Feature: Select Vendors
 
-Background: vendors
-  Given date is '15/7/2024' and time is'6-8'
+  Background: vendors
+    Given date is '15/7/2024' and time is'6-8'
 
   Scenario Outline: successfully add vendors
-    When user choose vendors for'<decoration>''<DJ>''<photographer>'
+    When user choose vendors  '<type>'
     Then book vendor to the event
     Examples:
-      | decoration                    | DJ                   | photographer                   |
-      |decoration@serviceprovider.com |DJ@serviceprovider.com|photographer@serviceprovider.com|
-
+      | type       |
+      |decoration  |
+      |DJ          |
+      |photographer|
---
+++ src/main/java/org/example/VendorsByUser.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class VendorsByUser{
+
+    public static String email;
+    public static String type;
+    public static int price;
+
+    public static String username;
+
+    public static int date;
+    public static int time;
+    public static boolean addSuccess;
+
+    public static int vendor_type;
+
+    ArrayList<VendorsByUser> vendors = new ArrayList<>();
+    public void getVendorsfromFile() {
+        try {
+
+            vendors.clear();
+            File file = new File("src/vendor.txt");
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                String EmailPriceType;
+                while ((EmailPriceType = bufferedReader.readLine()) != null) {
+                    String[] data = EmailPriceType.split(",");
+                    email=data[0];
+                    price=Integer.parseInt(data[1]);
+                    type=data[2];
+                    vendors.add(this);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public void writeVendors(String username, int date, int time,String email,String type) {
+        try {
+            File file = new File("src/waitList.txt");
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
+                String vendor = username + "," + date + "," + time + ","+email + "," + type+"\n";
+
+                bufferedWriter.write(vendor);
+                System.out.println(vendor);
+                addSuccess = true;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+    }
+
+    public void addvendor (String type){
+        getVendorsfromFile();
+        if ( type.contains ( "decoration" ) ){
+            vendor_type = 1;
+            writeVendors (username , date , time ,email,type );
+        }
+        else if (type.contains("DJ")){
+            vendor_type = 2 ;
+            writeVendors (username , date , time ,email,type );
+        }
+        else if (type.contains("photographer")){
+            vendor_type = 3 ;
+            writeVendors (username , date , time ,email,type);
+        }
+        else{
+            vendor_type=-1;
+        }
+    }
+}
